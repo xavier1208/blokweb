52443af3b0b0467b008e9bc6ddbc532353f5c18f
xavier1208 <94308919+xavier1208@users.noreply.github.com>
2021-12-08 08:44:53 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
 // JavaScript Document
+var hamMenu =
+document.querySelector("header nav ul li ul")
+
+var hamButton =
+document.querySelector(".icon");
+
+
+
+hamButton.addEventListener("click", toggleHamburgerMenu);
+
+function toggleHamburgerMenu(){
+    hamMenu.classList.toggle("visible");
+}
+
+
+
+
+
+var colorModeInput = document.querySelector("input");
+
+colorModeInput.addEventListener('change', erisopmijgeklikt);
+
+function erisopmijgeklikt() {
+  if( colorModeInput.checked ) {
+    document.documentElement.classList.add("lightMode");
+  } else {
+    document.documentElement.classList.remove("lightMode");
+  }
+}
